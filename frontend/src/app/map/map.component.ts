b4b4143f61cd023aa4d59fc6ab9995fc7f11ebea
Akiir3 <erba0445@colorado.edu>
2020-03-30 09:38:51 -0600

    Cleaning files after adding map

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
-=======
-import { Component, OnInit, APP_ID } from '@angular/core';
-import { YelpService} from '../yelp.service';
-import { Business} from '../businessModel';
-
->>>>>>> 33d806cc1bde064d0fc99dbdc8e7c7621d68b5aa
 
 @Component({
   selector: 'app-map',
@@ -15,7 +8,6 @@ import { Business} from '../businessModel';
 })
 export class MapComponent implements OnInit {
 
-<<<<<<< HEAD
   latitude: number;
   longitude: number;
   zoom:number;
@@ -23,7 +15,7 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  	this.setCurrentLocation();
+    this.setCurrentLocation();
   }
 
 // Get Current Location Coordinates
@@ -34,32 +26,6 @@ private setCurrentLocation() {
       this.longitude = position.coords.longitude;
       this.zoom = 15;
     });
-=======
-  business: Business[] = [];
-
-  title = 'frontend';
-
-
-  constructor(public yelpService : YelpService) { }
-
-  getSearchBuiness(): void {
-    this.yelpService.getSearchBuiness()
-      .subscribe(data => {
-        this.business = data;
-        console.log(this.business);
-      },
-      error => {
-        console.log(error);
-      });
-  }
-
-
-
-
-  ngOnInit() {
-   
-    this.getSearchBuiness();
->>>>>>> 33d806cc1bde064d0fc99dbdc8e7c7621d68b5aa
   }
 }
 
