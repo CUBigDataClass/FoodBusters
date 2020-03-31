a0aa8e5c3ae9137d94938bea36735d98e06c3167
Akiir3 <erba0445@colorado.edu>
2020-03-31 14:47:13 -0600

    Changes to leaflet Map

--- frontend/src/app/leaf-map/leaf-map.component.ts
+++ frontend/src/app/leaf-map/leaf-map.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+import { YelpService } from '../yelp.service';
+import { Business } from '../businessModel';
 
 @Component({
   selector: 'app-leaf-map',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeafMapComponent implements OnInit {
 
-  constructor() { }
+  //crete object for business
+  business: Business[] = [];
+  private map: L.Map;
+  
+  markers: L.Marker[];
 
-  ngOnInit(): void {
+  constructor(public yelpService : YelpService) { }
+
+  
+ 
+
+  getSearchBuiness(city): void {
+  this.yelpService.getSearchBuiness(city)
+    .subscribe(data => {
+      this.business = data;
+      console.log(this.business);
+    },
+    error => {
+      console.log(error);
+    });
+}
+
+  private initMap(): void {
+      // Setting location to Boulder
+      this.markers = [];
+      var p1 = L.latLng(40.149152, -105.378020),
+      p2 = L.latLng(39.957245, -105.170137),
+      bounds = L.latLngBounds(p1, p2);
+      this.map = L.map('map', {
+        // maxBounds: bounds
+      }).setView([40.0150, -105.2705], 12.5);
+  
+   
+      var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+      }).addTo(this.map)
   }
+
+
+  ngOnInit() {
+    this.initMap();
+    // this.getSearchBuiness('boulder');
+
+  }
+
 
 }
