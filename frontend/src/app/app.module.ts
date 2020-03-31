cf6e0d8d03f3154f21405f8456e40b4f9fbf2e5c
Akiir3 <erba0445@colorado.edu>
2020-03-31 12:13:21 -0600

    Deleting all GoogleAPI changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,34 +4,32 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { AgmCoreModule } from '@agm/core';
 
 
 
 @NgModule({
+
   declarations: [
     AppComponent,
-    MapComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyA8KjAWFi0vWlwGrYxfJnwvafEgotdLVwU',
-      libraries: ['places']
-    })
   ],
+
   exports:[
     MatToolbarModule,
     HttpClientModule,
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
