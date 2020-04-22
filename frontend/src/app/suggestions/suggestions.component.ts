b628779a725f169c11e4422278c42667adaa4b65
Akiir3 <erba0445@colorado.edu>
2020-04-22 17:38:57 -0600

    changes on suggestions commponent

--- frontend/src/app/suggestions/suggestions.component.ts
+++ frontend/src/app/suggestions/suggestions.component.ts
@@ -20,22 +20,21 @@ export class SuggestionsComponent implements OnInit {
     this.city = 'boulder'
    }
 
-  
-
   getSearchBusiness(city){
    
+    this.business = this.cityClickService.getBusinessService();
     console.log('get business for suggestion ', this.business);
+    this.top3Businesses = this.business.slice(0,3);
 
     this.sortBusinessesByRating();
-    // this.top3Businesses = this.business.slice(0,3);
-    // console.log('Top3Businesses: ',this.top3Businesses);
   }
 
 
-  getSuggestions(): void{
+  getSuggestions(city): void{
     this.city = this.cityClickService.getCity();
     console.log('city in suggestion ', this.cityClickService.getCity())
     this.getSearchBusiness(this.city);
+    return this.getSearchBusiness(this.city);
   }
 
   private sortBusinessesByRating(): void {
@@ -53,7 +52,7 @@ export class SuggestionsComponent implements OnInit {
 
   ngOnInit(): void {
     
-    this.getSuggestions();
+    this.getSuggestions('boulder');
     
   }
 
